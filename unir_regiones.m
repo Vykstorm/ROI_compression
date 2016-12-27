107150f91d683107a705f056052d09190760cd1b
Vykstorm <victorruizgomezdev@gmail.com>
2016-12-27 12:47:20 +0100

    Añadida función para poder reconstruir una imágen uniendo regiones o partes de esta. Será útil a la hora de reconstruir la imágen original con las regiones una vez descomprimidas

---
+++ unir_regiones.m
@@ -0,0 +1,20 @@
+
+% Autor: Víctor Ruiz Gómez
+% Descripción: Este método, a partir de las regiones obtenidas al subdividir 
+% la imágen original, es capaz de reconstruir dicha imágen, uniendo las 
+% distintas regiones.
+% Parámetros: Un array de celdas R que contiene información de las distintas
+% regiones (es igual que el valor de retorno de la función regiones).
+% También debe indicarse el tamaño S de la imágen original.
+% Valor de retorno: La imágen resultante de unir las distintas regiones.
+function [ M ] = unir_regiones(R, S)
+	M = zeros(S);
+	k = size(R,1); % nº regiones.
+	for i=1:k % Por cada región..
+		p = R{i,1};
+		x = p(1); y = p(2); % Posición de la región.
+		pixels = R{i,2}; % Pixeles de la región.
+		[n,m] = size(pixels);
+		M(x:x+n-1, y:y+m-1) = pixels;
+	end;
+end
