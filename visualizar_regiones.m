14be1ebe7b87a97a0ab8a539a81ffe8119bde5a7
Vykstorm <victorruizgomezdev@gmail.com>
2016-12-27 12:46:09 +0100

    Añadido método para poder depurar la división de la imágen en regiones

---
+++ visualizar_regiones.m
@@ -0,0 +1,22 @@
+
+% Autor: Víctor Ruiz Gómez
+% Descripción: Este método permite visualizar las regiones obtenidas al
+% decomponer una imágen.
+% Toma como parámetro la imágen original y la información sobre la división de
+% la imágen en regiones (valor de retorno del método regiones)
+
+function visualizar_regiones (M, R)
+	
+	figure;
+	imshow(uint8(M));
+	hold on;
+	
+	k = size(R,1);
+	for i=1:k  % Mostramos cada región..
+		p = R{i,1};
+		x = p(1); y = p(2);
+		[n,m] = size(R{i,2});
+		rectangle('Position', [x,y,n,m], 'LineWidth', 2, 'EdgeColor', 'g');
+		
+	end;
+end;
