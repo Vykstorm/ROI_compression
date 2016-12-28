be1be2ba8bbe193a3f02ac836faaa6733ff2688f
Vykstorm <victorruizgomezdev@gmail.com>
2016-12-28 14:54:04 +0100

    Arreglado error en el método frequencies

--- utility/frequencies.m
+++ utility/frequencies.m
@@ -3,13 +3,14 @@
 % Devuelve como retorno, por un lado, un vector columna Y con los datos
 % que aparecen al menos una vez en X y otro vector columna H con su frecuencia
 % correspondiente
-function [Y, H] = hist(X)
+function [Y, H] = frequencies(X)
 	X = reshape(X, 1, []);
+	n = prod(size(X));
 	Y = unique(X)';
 	k = length(Y);
 	
 	H = zeros(size(Y,1),1);
 	for i=1:k
-		H(i) = sum(X == Y(i)) / k;
+		H(i) = sum(X == Y(i)) / n;
 	end;
 end
