6f5e5f9033efa552875b50f7af872b44575c19a7
Vykstorm <victorruizgomezdev@gmail.com>
2016-12-28 18:49:32 +0100

    Ahora el método regiones devuelve como resultado, para cada región idenfiticada, los valores del mapa usados por el criterio de descomposición, para determinar si una región debe subdividirse o no

--- regiones.m
+++ regiones.m
@@ -16,17 +16,17 @@
 % aquellas regiones M que satisfagan f(M) = 0
 
 % Parámetros de entrada:
-% - Imágen a subdividir M
-% - M2 es la imágen de la que posteriormente se extraeran los pixeles de las regiones,
-% por si M ~= M2 (tanto M como M2 deben tener las mismas dimensiones)
+% - M es un mapa de valores que se usaráa para determinar si
+% una región debe subdividirse más o no.
+% - M2 es la imágen real de la que posteriormente se extraeran los pixeles de las regiones,
+%  si M ~= M2 (tanto M como M2 deben tener las mismas dimensiones)
 % - Criterio de división F(M) -> {0, 1} Es 1 si M debe subdividirse, 0 en caso contrario.
-% 
-
-% Valor de retorno: (proporciona información sobre la divisón en regiones realizada...)
-% - Un array de celdas (rx2) donde r es el número de regiones en las que se ha dividido la
+% % Valor de retorno: (proporciona información sobre la divisón en regiones realizada...)
+% - Un array de celdas (rxe) donde r es el número de regiones en las que se ha dividido la
 % imágen. La primera columna de cada región indica en que posición se encuentra, 
-% en relación con la imágen original (x,y) es un vector de 2 componentes. 
-% La segunda columna es una matriz con los pixeles de la región.
+% en relación con la imágen original (x,y) (es un vector de 2 componentes)
+% La segunda columna es una matriz con los pixeles de la región correspondiente.
+% La tercera, es una submatriz del mapa M de la región correspondiente.
 function [R] = regiones(M, M2, f)
 	[n,m] = size(M);
 	F = @(D) cell2mat(slicefun(D, f));
@@ -36,7 +36,7 @@ function [R] = regiones(M, M2, f)
 	P = [floor((P-1)/m) + 1; mod(P-1, m) + 1]';
 	k = size(P,1); % nº regiones identificadas (hojas del árbol quadtree)
 	
-	R = cell(k, 2);
+	R = cell(k, 3);
 	R(:,1) = mat2cell(P, ones(1,k), 2);
 	
 	% Obtenemos los píxeles de cada región...
@@ -45,5 +45,6 @@ function [R] = regiones(M, M2, f)
 		l = S((x-1)*m + y);  % Tamaño de la región
 		pixels = M2(x:x+l-1, y:y+l-1, :); % Píxeles de la región.
 		R{i,2} = pixels;
+		R{i,3} = M(x:x+l-1, y:y+l-1);
 	end;
 end 
