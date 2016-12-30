2832b4db4f2b7e10cff9806c8e09d2a03595008e
Vykstorm <victorruizgomezdev@gmail.com>
2016-12-30 21:06:11 +0100

    Ahora la transformada del coseno se puede aplicar por bloques en cada region. Se añade un parámetro para indicar el tamaño de bloque

--- comprimir.m
+++ comprimir.m
@@ -3,8 +3,26 @@
 % trunca paarte de los píxeles (después de haber aplicado la transformada), en base
 % al ratio de compresión indicado.
 % Posteriormente, se descomprime la imágen (aplicando la inversa de la transformada)
-% el nº píxeles truncados es s-floor(s/r) donde r es el ratio de compresión, s es el
-% n total de píxeles.
-function [M2] = comprimir(M, r)
-	M2 = M;
+% el nº píxeles truncados total es floor(sqrt((s - s / r) / (n/k)^2))^2 * (n/k)^2  donde r es el ratio de compresión, s es el
+% n total de píxeles, y k es el tamaño de bloque (se divide el conjunto de pixeles en bloques y se aplica
+% la transformada por cada bloque de forma individual)
+function [M2] = comprimir(M, r, k)
+	M2 = zeros(size(M));
+	d = size(M,3);
+	n = size(M,1);
+	s = prod(size(M))/d;
+
+	% Aplicar la transformada por cada componente de color..
+	for j=1:d
+		C = M(:,:,j);
+		
+		% Aplicar la transformada por cada bloque
+		C = blockproc(C, [k,k], @dct2);
+	
+		% Truncar píxeles con menos información en base al ratio de compresión por cada bloque
+		C = blockproc(C, [k,k], @(X) truncate(X, repmat(floor(sqrt((s-s/r)/(n/k)^2)), 1, 2), 0));
+		
+		% Aplicar la inversa de la transformada por cada bloque
+		M2(:,:,j) = blockproc(C, [k,k], @idct2);
+	end;
 end
