6afe31f9b68d7d53460bfac953f6a10e0c3c0581
Vykstorm <victorruizgomezdev@gmail.com>
2016-12-31 20:32:59 +0100

    Modificado algoritmo de optimizacion para calculo de ratios de compresion

--- calcular_ratios.m
+++ calcular_ratios.m
@@ -3,37 +3,24 @@
 % Toma como parámetro el ratio de compresión global que se quiere obtener al comprimir la imágen,
 % los niveles de compresión (L) de cada región de la imágen (en el rango [0, 1) ), y el nº de pixeles
 % de cada región (S).
-% Devuelve los ratios de compresión locales de cada región.
-% Se calculan de la siguiente forma:
-% l1, l2, ..., ln son los niveles de compresión en el rango [0, 1)
-% s1, s2, ..., sn es el nº de pixeles de cada región.
-% Se quiere calcular r1, r2, ..., rn que serán los ratios locales.
-% Se crea un sistema lineal no determinado con las siguientes premisas:
-% 				l1/l2 = r1/r2, l1/l3 = r1/r3, l1/l4 = r1/r4, ..., l1/ln = r1/rn
-% 							    l2/l3 = r2/r3, l2/l4 = r2/r4 ..., l2/ln = r2/rn
-% ...
-% y también debe cumplirse que:
-
-% cr = (s1+s2+...+sn) / (s1/r1 + s2/r2 + ...+ sn/rn)
+% Devuelve los ratios de compresión locales de cada región..
 
 function [LCR] = calcular_ratios(cr, L, S)
 	nr = length(L);
-	C = combnk(1:nr, 2);
+
+	lb = 1 ./ S;
+	ub = ones(nr,1);
 	
-	% Creamos el sistema lineal no determinado.
-	T = zeros(nr+1, nr);
+	Aeq = ones(1,nr);
+	beq = sum(S) / cr;
 	
-	for i=1:nr
-		a = C(i,1); b = C(i,2);
-		T(i,:) = [repmat(0, [1 a-1]), 1, repmat(0, [1 b-a-1]), -L(b)/L(a), repmat(0, [1 nr-b])];
-	end;
+	C = eye(nr);
+	d = 1 ./ L;
 	
-	T(nr+1,:) = S;
-	d = [zeros(nr,1); sum(S)/cr];
-	LCR = 1./lsqnonneg(T, d);
-	
-	% Reajustamos los ratios (algunos ratios no son válidos)
+	%LCR = 1./lsqnonneg(T, d);
+	LCR = (1./lsqlin(C, d, [], [], Aeq, beq, [], [])) .* S;
+
+	% Reajustamos los ratios (por si algunos ratios no son válidos)
 	LCR = bsxfun(@min, bsxfun(@max, LCR, 1), S);
 end;
-
 
