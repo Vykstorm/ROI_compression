897aedc1e2cca6d54b603541c4467fd10e6cdb01
Vykstorm <victorruizgomezdev@gmail.com>
2016-12-31 12:28:06 +0100

    Se modifica el calculo del nº pixeles que se deben truncar por bloque

--- comprimir.m
+++ comprimir.m
@@ -3,7 +3,7 @@
 % trunca paarte de los píxeles (después de haber aplicado la transformada), en base
 % al ratio de compresión indicado.
 % Posteriormente, se descomprime la imágen (aplicando la inversa de la transformada)
-% el nº píxeles truncados total es floor(sqrt((s - s / r) / (n/k)^2))^2 * (n/k)^2  donde r es el ratio de compresión, s es el
+% el nº píxeles truncados total es ceil(sqrt((s - s / r) / (n/k)^2))^2 * (n/k)^2  donde r es el ratio de compresión, s es el
 % n total de píxeles, y k es el tamaño de bloque (se divide el conjunto de pixeles en bloques y se aplica
 % la transformada por cada bloque de forma individual)
 function [M2] = comprimir(M, r, k)
@@ -20,7 +20,7 @@ function [M2] = comprimir(M, r, k)
 		C = blockproc(C, [k,k], @dct2);
 	
 		% Truncar píxeles con menos información en base al ratio de compresión por cada bloque
-		C = blockproc(C, [k,k], @(X) truncate(X, repmat(floor(sqrt((s-s/r)/(n/k)^2)), 1, 2), 0));
+		C = blockproc(C, [k,k], @(X) truncate(X, repmat(ceil(sqrt((s-s/r)/(n/k)^2)), 1, 2), 0));
 		
 		% Aplicar la inversa de la transformada por cada bloque
 		M2(:,:,j) = blockproc(C, [k,k], @idct2);
