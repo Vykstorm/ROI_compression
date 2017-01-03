6dd714d3f95d20e348e0d1c75de2375f384e93de
Vykstorm <victorruizgomezdev@gmail.com>
2017-01-03 21:27:21 +0100

    Se reimplementa el algoritmo de optimización para calculo de ratios de compresión locales

--- calcular_ratios.m
+++ calcular_ratios.m
@@ -7,19 +7,42 @@
 
 function [LCR] = calcular_ratios(cr, L, S)
 	nr = length(L);
+	
+	%% Solucionamos un problema de optimización.
+	% r1, r2, ..., rn son los ratios de compresión local a calcular.
+	% l1, l2, ..., ln son los niveles de compresión de cada región.
+	% s1, s2, ...., sn es el nº pixeles por cada región.
+	% cr es el ratio global
+	% Resolvemos un algoritmo de optimización. 
+	% Partimos del punto inicial de que los ratios 
+	% r1, r2, ..., rn = 1 / (1 - l1), 1 / (1 - l2), ..., 1 / (1 - ln)
+	% esta es la situación ideal, pero queremos que..
+	% suma i=1...n, si / ri  se aproxime a  (sumai=1..n si) / rc 
+	% con la condición de que  r1 <= 1 / (1 - l1), r2 <= 1 / (1 - l2), ...
+	% Calculamos los coeficientes k1, k2, ..., kn tal que se minimize
+	% el siguinte error:
+	% 1/2 ([suma i=1...n(si / (1 - (1 - li/ki)))] - [(sumai=1..n si) / rc]) ^ 2
 
-	lb = 1 ./ S;
-	ub = ones(nr,1);
-	
-	Aeq = ones(1,nr);
-	beq = sum(S) / cr;
-	
-	C = eye(nr);
-	d = 1 ./ L;
-	
-	%LCR = 1./lsqnonneg(T, d);
-	LCR = (1./lsqlin(C, d, [], [], Aeq, beq, [], [])) .* S;
 
+	% Comenzamos la búsqueda indicando el punto inicial R0
+	K0 = ones(nr,1);
+	
+	% Valor mínimo de K que queremos encontrar
+	lb = ones(nr,1);
+
+	% Función F que devuelve un conjunto de valores F1, F2, ..., FN
+	% Al final minimizaremos la suma de los cuadrados de estos valores ->
+	% minimizar F1^2 + F2^2 + ... + Fm^2
+	
+	alpha = 20;
+	F = @(K) alpha * abs((sum(S ./ (1 ./ (1 - (L ./ K)))) - (sum(S) / cr))) / sum(S-1);
+
+
+	opts = optimset('Algorithm', 'trust-region-reflective', 'MaxIter', 1000);
+	K = lsqnonlin(F, K0, lb, [], opts);
+
+	LCR = 1 ./ (1 - (L ./ K));
+	
 	% Reajustamos los ratios (por si algunos ratios no son válidos)
 	LCR = bsxfun(@min, bsxfun(@max, LCR, 1), S);
 end;
